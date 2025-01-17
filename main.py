3863665668233428f5808007ead86b4c39d7af5c
Noelle Rangel <192288701+NoelleRangel@users.noreply.github.com>
2025-01-17 04:04:06 +0000

    Pushed most age related things into my_age_function to organize.

--- main.py
+++ main.py
@@ -4,18 +4,22 @@ Welcome the user, collect the users name and age, ask the user how it can help t
  the user to choose from a menu/list of options on how they can continue the conversation.
 """
 
+def my_age_function(age):
+    if age > 0 and age < 18:
+        print(f'Oh my, {age}? Trying to start early huh?')
+    elif age > 17 and age < 35:
+        print(f'{age} was such a year to be alive...')
+    elif age > 34 and age < 65:
+        print(f'Everyone knows {age} is prime time! ')
+    elif age > 64:
+        print(f'Wow, {age}? Keep on kicking!')
+
+
 print('Welcome to the ELite 101 chatbot project!')
 name = input('What should we refer to you as? ')
 age = int(input(f'Hello {name}! How old are you? '))
+my_age_function(age)
 
-if age > 0 and age < 18:
-    print(f'Oh my, {age}? Trying to start early huh?')
-elif age > 17 and age < 35:
-    print(f'{age} was such a year to be alive...')
-elif age > 34 and age < 65:
-    print(f'Everyone knows {age} is prime time! ')
-elif age > 64:
-    print(f'Wow, {age}? Keep on kicking!')
 
 print('\nPlease take a look at our following services below: ')
 print('1. Placeholder one')
@@ -31,5 +35,5 @@ elif choice == 2:
 elif choice == 3:
     print('Placeholder three')
 elif choice == 4:
-    print(f'Thank you for using the program, goodnight {name}! ')
+    print(f'Thank you for using the program, goodbye {name}! ')
     #ends the program.
